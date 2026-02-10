bdfe0cb776f64667fcd24e1927c1e669823da065
pavan-progamming <pavankumarrallapati9182@gmail.com>
2026-02-10 18:46:25 +0530

    10/2/26

---
+++ day1.cpp
@@ -0,0 +1,56 @@
+// // Basic syntex
+// #include <iostream>  // including the c++
+// using namespace std; // used to std ::
+// int main()
+// {                            // starting the main method
+//     cout << "hello" << endl; // printing the output
+//     return 0;
+// }
+
+// //Taking the input from the user
+// #include <iostream>
+// using namespace std;
+// main() {
+//     string a ;
+//     cout << "Enter your name :- ";
+//     cin >> a ;
+//     cout << "hello " << a << " welcome to Brain_To_Code " ;
+// }
+
+
+// // Variables and datatypes
+// #include <iostream>
+// #include <typeinfo>
+// using namespace std;
+// main()
+// {
+//     int a = 10;
+//     char b = 'c';
+//     string name = "brain_to_code";
+//     float c = 22.0;
+//     cout << a << endl;
+//     cout << b << endl;
+//     cout << c << endl;
+// }
+
+// Arthimatic operations 
+#include <iostream>
+using namespace std;
+main() {
+    int a;
+    int b;
+    cout << "enter num one :- " ;
+    cin  >> a ;
+    cout << "enter num two :- " ;
+    cin  >> b; 
+    cout << "the sum is :- " << a + b << endl;
+    cout << "the difference is :- " << a - b << endl;
+    cout << "the product is :- " << a * b << endl;
+    cout << "the division is :- " << a / b << endl;
+
+}
+
+
+
+
+
---
+++ day2.cpp
@@ -0,0 +1,64 @@
+// // conditional statements in c++
+// #include <iostream>
+// using namespace std;
+// main() {
+//     int a ;
+//     cout << "Enter Your age :- " ;
+//     cin >> a ;
+//     if (a <= 18) {
+//         cout << "You are not eligible to vote !";
+//     } else {
+//         cout << "You are eligible to vote ";
+//     }
+// }
+
+// nested conditional statements 
+// #include <iostream>
+// using namespace std;
+// main() {
+//     int a ;
+//     cout << "Enter Your Age :- " ;
+//     cin >> a ;
+//     if (a>18) {
+//         cout << "Congrates! you are eligible to vote "  ; 
+//     } if (a==18) {
+//         cout << "you still need an Year to vote ";
+//     } else {
+//         cout << "You are not eligible to vote ";
+//     }
+// }
+
+// Loops in c++
+
+// Basic loop 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//         int a ;
+//         for (int i=0; i<=10; i++) {
+//             cout <<"\n"<< i ;
+//         }
+// }
+
+//for with if 
+// #include <iostream>
+// using namespace std ;
+// main (){
+//     for (int i =0 ;i<=10 ;i++) {
+//          if ( i == 5) {
+//             cout << "Your time is done ";
+//         }
+//     }
+// }
+
+// while loop 
+// #include <iostream>
+// using namespace std;
+// main() {
+//     int i=0;
+//     while (i<=10) {
+//         cout << i;
+//         i++;
+//     }
+    
+// }
