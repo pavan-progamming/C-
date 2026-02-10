93241692b36c55394e58a6d352d3c0525eea76d4
pavan-progamming <pavankumarrallapati9182@gmail.com>
2026-02-10 18:52:04 +0530

    removing code from commernt

--- day1.cpp
+++ day1.cpp
@@ -1,37 +1,37 @@
 // // Basic syntex
-// #include <iostream>  // including the c++
-// using namespace std; // used to std ::
-// int main()
-// {                            // starting the main method
-//     cout << "hello" << endl; // printing the output
-//     return 0;
-// }
+#include <iostream>  // including the c++
+using namespace std; // used to std ::
+int main()
+{                            // starting the main method
+    cout << "hello" << endl; // printing the output
+    return 0;
+}
 
 // //Taking the input from the user
-// #include <iostream>
-// using namespace std;
-// main() {
-//     string a ;
-//     cout << "Enter your name :- ";
-//     cin >> a ;
-//     cout << "hello " << a << " welcome to Brain_To_Code " ;
-// }
+#include <iostream>
+using namespace std;
+main() {
+    string a ;
+    cout << "Enter your name :- ";
+    cin >> a ;
+    cout << "hello " << a << " welcome to Brain_To_Code " ;
+}
 
 
 // // Variables and datatypes
-// #include <iostream>
-// #include <typeinfo>
-// using namespace std;
-// main()
-// {
-//     int a = 10;
-//     char b = 'c';
-//     string name = "brain_to_code";
-//     float c = 22.0;
-//     cout << a << endl;
-//     cout << b << endl;
-//     cout << c << endl;
-// }
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+main()
+{
+    int a = 10;
+    char b = 'c';
+    string name = "brain_to_code";
+    float c = 22.0;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+}
 
 // Arthimatic operations 
 #include <iostream>
